f08abed8a8baf4126a81226212393e24129740b3
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2024-09-22 12:37:21 +0100

    Updated Key Component, Added event listeners

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -2,18 +2,38 @@ import clsx from "clsx";
 import { NoteType } from "../../domain/note";
 import styles from "./Key.module.css";
 import { ReactElement } from "react";
+import { usePressObserver } from "../PressObserver";
 
+type PressCallback = () => void;
 export interface KeyProps {
   type: NoteType;
   label: string;
   disabled?: boolean;
+  onUp: PressCallback;
+  onDown: PressCallback;
 }
 
 export const Key = (props: KeyProps): ReactElement => {
-  const { type, label, ...rest } = props;
+  const { type, onDown, onUp, label, ...rest } = props;
+
+  const pressed = usePressObserver({
+    watchKey: label,
+    onFinishPress: onUp,
+    onStartPress: onDown,
+  });
 
   return (
-    <button className={clsx(styles.key, styles[type])} type="button" {...rest}>
+    <button
+      className={clsx(
+        styles.key,
+        styles[type],
+        pressed && styles["is-pressed"]
+      )}
+      type="button"
+      onMouseDown={onDown}
+      onMouseUp={onUp}
+      {...rest}
+    >
       {label}
     </button>
   );
