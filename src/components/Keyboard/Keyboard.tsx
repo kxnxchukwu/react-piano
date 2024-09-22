a809d8f071adf41cf8a634be1ae3cfa8c71b0c4b
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2024-09-22 12:38:11 +0100

    Added KeyboardProps and passed props down to Key

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,15 +1,34 @@
-import { ReactElement } from "react";
+import { ReactElement, lazy } from "react";
 import { Key } from "../Key";
 import { selectKey } from "../../domain/keyboard";
-import { notes } from "../../domain/note";
+import { MidiValue, notes } from "../../domain/note";
 import styles from "./Keyboard.module.css";
 
-export const Keyboard = (): ReactElement => {
+export interface KeyboardProps {
+  loading: boolean;
+  play: (note: MidiValue) => Promise<void>;
+  stop: (note: MidiValue) => Promise<void>;
+}
+
+export const Keyboard = ({
+  loading,
+  play,
+  stop,
+}: KeyboardProps): ReactElement => {
   return (
     <div className={styles.keyboard}>
       {notes.map(({ midi, type, index, octave }) => {
         const label = selectKey(octave, index);
-        return <Key key={midi} type={type} label={label} />;
+        return (
+          <Key
+            key={midi}
+            type={type}
+            label={label}
+            disabled={loading}
+            onDown={() => play(midi)}
+            onUp={() => stop(midi)}
+          />
+        );
       })}
     </div>
   );
